57deef1374dfef6a3f7b653755d8f1eb32be1325
hellosiyam <hellosiyambd@gmail.com>
2025-02-15 17:23:59 +0600

    mi-8-mo-45-react-practices

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,13 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 const Error = () => {
     const error = useRouteError()
+    const navigate = useNavigate()
+    const backToTheHome = () =>{
+        navigate('/')
+    }
     return (
         <div>
             <h1>Oops!!!</h1>
             <p>{
                 error.statusText || error.massage
             }</p>
+            <button onClick={backToTheHome}>Go back Home</button>
         </div>
     );
 };
